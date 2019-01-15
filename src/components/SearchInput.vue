7d72f46fb25c9a609c089d2263ea4bf593092c99
damianVue <damian.janus@gmail.com>
2019-01-15 23:26:43 +0100

    Style mobile version

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -21,10 +21,20 @@ export default {
     width: 250px;
 
     input {
+      color: white;
+      text-align: center;
+      font-size: 18px;
+      font-weight: 300;
       height: 30px;
       border: 0;
       background: none;
-      border-bottom: 1px solid black;
+      border-bottom: 1px solid white;
+      transition: box-shadow .5s;
+    }
+
+    input:focus {
+      outline: none;
+      box-shadow: 0 10px 20px -8px rgba(255, 255, 255, .3);
     }
   }
 </style>
